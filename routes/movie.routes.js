3c79d494b12cd76d9ef8c964d61044aa9ed971e3
ajaymohare <ajayyynot31@gmail.com>
2026-08-15 05:27:40 +0530

    movie.routes.js → endpoints for movies

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const movieController = require('../controllers/movie.controller');
 const showtimeController = require('../controllers/showtime.controller');
 const { verifyToken, isAdmin } = require('../middleware/auth');
-
 // Movie routes
 router.post('/', verifyToken, isAdmin, movieController.createMovie);
 router.get('/', movieController.getAllMovies);
